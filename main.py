b80ce4495847d2fe65b61f21af41e69f2d6ba16c
Afterburn <mujahideen.solutions@gmail.com>
2014-07-08 07:45:36 -0600

    register

--- main.py
+++ main.py
@@ -6,21 +6,79 @@ from flask import session
 from flask import url_for
 from flask import escape
 from flask.ext.sqlalchemy import SQLAlchemy
-
-
+from flask.ext.login import LoginManager
 
 import os
 from libs.utils import Utils
 from libs.pi    import Pi
 
+import evelink.api
+
+eve = evelink.eve.EVE()
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = '%s/user_data.db' % (os.getcwd())
 db = SQLAlchemy(app)
 
+login_manager  = LoginManager()
+login_manager.init_app(app)
+
+# temporary
+CORP_ID = 98301190
+
+
+@login_manager.user_loader 
+def load_user(userid):
+    return None
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+
+    if request.method == 'POST':
+        # debug
+        print(request.form)
+
+
+        api = evelink.api.API(api_key=(request.form.get('register_keyid'), request.form.get('register_code')))
+        account = evelink.account.Account(api)
+
+        characters = account.characters()
+
+
+        if characters.result:
+
+            # Remove characters that are not in the corp.
+            for character in characters.result:
+                if characters.result[character]['corp']['id'] != CORP_ID:
+                    del characters.result[character]
+               
+            # Check to see if we still have any characters left. 
+            if characters.result:
+                print(characters.result)
+                session['characters'] = characters.result
+
+            return redirect(url_for('confirm_register'))
+
+
+    return render_template('register.html')
+
+
+@app.route('/confirm_register', methods=['POST', 'GET'])
+def confirm_register():
+    if request.method == 'POST': 
+        # debug 
+        print('[confirm_register] %s' % (request.form))
+
+        return render_template('submitted_register.html')
+        
+
+    return render_template('confirm_register.html', characters=session['characters'])
+
 
 
 #def index():
